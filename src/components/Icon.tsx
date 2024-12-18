def75ce94c90144ebba41e6334d6e5a3bda53a5f
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-18 18:47:03 +0530

    feat : added icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+type IconType = "arrow-up" | "arrow-down" | "trending-up";
+
+interface IconProps {
+  type: IconType;
+  className?: string;
+  size?: number;
+}
+
+const Icon: React.FC<IconProps> = ({ type, className = "", size = 24 }) => {
+  const icons = {
+    "arrow-up": (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        width={size}
+        height={size}
+        className={className}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
+        />
+      </svg>
+    ),
+    "arrow-down": (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        width={size}
+        height={size}
+        className={className}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+        />
+      </svg>
+    ),
+    "trending-up": (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        width={size}
+        height={size}
+        className={className}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
+        />
+      </svg>
+    ),
+  };
+
+  return icons[type];
+};
+
+export default Icon;
