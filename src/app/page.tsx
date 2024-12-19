8f8cb2c76a4f9e0d6301d6dc987a989e211539ca
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 11:54:31 +0530

    feat : Made a seperate AtGalance component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,120 +8,19 @@ import Table from "@/components/Table";
 import ModuleNav from "@/app/components/ModuleNav";
 import Consulations from "@/app/components/Consulations";
 import PastPeriod from "@/app/components/PastPeriod";
-
-interface TitleBodyProps {
-  title?: string;
-  children?: React.ReactNode;
-  RightComponent?: React.ReactNode;
-}
-
-const TitleBody: React.FC<TitleBodyProps> = ({
-  title = "Title",
-  children,
-  RightComponent,
-}) => {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-between items-center">
-        <Typography variant="h4">{title}</Typography>
-        {RightComponent}
-      </div>
-      {children}
-    </div>
-  );
-};
-
+import OrdersTable from "./components/OrdersTable";
+import TitleBody from "@/components/TitleBody";
+import AtGalance from "@/app/components/AtGalance";
 export default function Home() {
   return (
     <div>
       <ModuleNav />
       <section className="flex flex-col gap-4 p-4">
         <div className="flex flex-col gap-4 rounded-lg p-4 border border-gray-200 shadow-sm">
-          <TitleBody title="At a glance" RightComponent={<div>Button</div>}>
-            <div className="flex flex-col gap-4">
-              <div className="flex gap-4">
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="plan-chat" size={12} />
-                      <Typography variant="caption">CONSULTATIONS</Typography>
-                    </div>
-                  }
-                  value="24"
-                  type="increase"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="price-tag" size={12} />
-                      <Typography variant="caption">ORDERS PLACED</Typography>
-                    </div>
-                  }
-                  value="24"
-                  type="decrease"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="check-fat" size={12} />
-                      <Typography variant="caption">CONVERSION</Typography>
-                    </div>
-                  }
-                  value="24"
-                  type="decrease"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-              </div>
-              <div className="flex gap-4">
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="coin-stack" size={12} />
-                      <Typography variant="caption">
-                        TOTAL SALES VALUE
-                      </Typography>
-                    </div>
-                  }
-                  value="$ 24,000"
-                  type="increase"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="coins" size={12} />
-                      <Typography variant="caption">AVG ORDER VALUE</Typography>
-                    </div>
-                  }
-                  value="24"
-                  type="increase"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-                <DataCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="piggy-bank" size={12} />
-                      <Typography variant="caption">COMMISSION PAID</Typography>
-                    </div>
-                  }
-                  value="$ 240"
-                  type="increase"
-                  percent={"100 %"}
-                  className="flex-1"
-                />
-              </div>
-            </div>
-          </TitleBody>
+          <AtGalance />
           <TitleBody title="Insights">
             <div className="flex flex-col gap-4">
-              <div className="flex gap-4">
+              <div className="flex flex-col gap-4 sm:flex-row">
                 <BaseCard
                   title={
                     <div className="flex flex-row gap-2 items-center">
@@ -179,7 +78,9 @@ export default function Home() {
             </div>
           </TitleBody>
           <TitleBody title="Orders">
-            <Table />
+            <div className="overflow-x-auto">
+              <OrdersTable />
+            </div>
           </TitleBody>
         </div>
       </section>
