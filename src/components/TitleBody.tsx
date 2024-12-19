a095fe961780f2f5cf0a394fcc1d7a5552c4330e
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 19:49:29 +0530

    added Title body section

--- src/components/TitleBody.tsx
+++ src/components/TitleBody.tsx
@@ -4,15 +4,17 @@ interface TitleBodyProps {
   title?: string;
   children?: React.ReactNode;
   RightComponent?: React.ReactNode;
+  className?: string;
 }
 
 const TitleBody: React.FC<TitleBodyProps> = ({
   title = "Title",
   children,
   RightComponent,
+  className,
 }) => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className={`flex flex-col gap-2 md:gap-4 ${className}`}>
       <div className="flex justify-between items-center">
         <Typography variant="h4">{title}</Typography>
         {RightComponent}
