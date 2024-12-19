945494e7bc3c4e01f95efb91153d4ce82f424aec
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 10:25:28 +0530

    added its own chat section

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,40 +1,16 @@
 "use client";
 
-import Typography from "@/components/Typography";
-import Table from "@/components/Table";
 import ModuleNav from "@/app/components/ModuleNav";
-
-interface TitleBodyProps {
-  title?: string;
-  children?: React.ReactNode;
-  RightComponent?: React.ReactNode;
-}
-
-const TitleBody: React.FC<TitleBodyProps> = ({
-  title = "Title",
-  children,
-  RightComponent,
-}) => {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-between items-center">
-        <Typography variant="h4">{title}</Typography>
-        {RightComponent}
-      </div>
-      {children}
-    </div>
-  );
-};
+import TitleBody from "@/components/TitleBody";
+import ChatsTable from "../components/ChatsTable";
 
 export default function Chats() {
   return (
     <div>
       <ModuleNav />
       <section>
-        <TitleBody title="Title" RightComponent={<div>Button</div>}></TitleBody>
-        <TitleBody title="Insights"></TitleBody>
-        <TitleBody title="Orders">
-          <Table />
+        <TitleBody title="Chats">
+          <ChatsTable />
         </TitleBody>
       </section>
     </div>
