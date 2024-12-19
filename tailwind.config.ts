9f92174e92bb618a04b1618d2daba17aaec240a2
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 11:53:10 +0530

    added flowbit plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite/plugin";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -19,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite],
 } satisfies Config;
