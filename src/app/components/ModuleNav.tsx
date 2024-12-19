77d3139b31d58b7af3f3c8a38ccbdb7384d3e976
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 11:56:08 +0530

    feat : made nav button scrollable on smaller screen

--- src/app/components/ModuleNav.tsx
+++ src/app/components/ModuleNav.tsx
@@ -20,29 +20,31 @@ const ModuleNav: React.FC<ModuleNavProps> = ({
   const pathname = usePathname();
 
   return (
-    <section className="flex items-center gap-2">
-      {modules.map((item, index) => {
-        const active = pathname === item.href;
-        return (
-          <div key={index} className="flex items-center gap-2">
-            <BaseButton
-              variant="pill"
-              as="a"
-              href={item.href}
-              className={`${active ? "bg-secondary" : "bg-transparent"}`}
-              leftIcon={item.icon}
-              leftIconColor={active ? "black" : "#8A94A6"}
-            >
-              <Typography
-                variant="body"
-                className={`${active ? "text-foreground" : "text-gray-500"}`}
+    <section className="flex items-center gap-2 p-4">
+      <div className="flex items-center gap-2 overflow-x-auto">
+        {modules.map((item, index) => {
+          const active = pathname === item.href;
+          return (
+            <div key={index} className="flex items-center gap-2">
+              <BaseButton
+                variant="pill"
+                as="a"
+                href={item.href}
+                className={`${active ? "bg-secondary" : "bg-transparent"}`}
+                leftIcon={item.icon}
+                leftIconColor={active ? "black" : "#8A94A6"}
               >
-                {item.title}
-              </Typography>
-            </BaseButton>
-          </div>
-        );
-      })}
+                <Typography
+                  variant="body"
+                  className={`${active ? "text-foreground" : "text-gray-500"}`}
+                >
+                  {item.title}
+                </Typography>
+              </BaseButton>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
