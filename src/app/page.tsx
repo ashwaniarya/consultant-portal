fb4279406f95153d2008b2caeb91c496d105ca17
Ashwani Arya <ashwaniparker@gmail.com>
2024-12-19 19:48:36 +0530

    remove complexity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,38 @@
 "use client";
 
-import DataCard from "@/components/Card";
-import Typography from "@/components/Typography";
-import { BaseCard } from "@/components/Card";
-import Icon from "@/components/Icon";
-import Table from "@/components/Table";
 import ModuleNav from "@/app/components/ModuleNav";
-import Consulations from "@/app/components/Consulations";
-import PastPeriod from "@/app/components/PastPeriod";
-import OrdersTable from "./components/OrdersTable";
-import TitleBody from "@/components/TitleBody";
 import AtGalance from "@/app/components/AtGalance";
+import Section from "@/components/Section";
+import { useState } from "react";
+import Insights from "./components/Insights";
+import Orders from "./components/Orders";
+
+const filterOptions = [
+  { value: "today", label: "Today" },
+  { value: "7 days", label: "7 days" },
+  { value: "1 month", label: "1 month" },
+];
+
 export default function Home() {
+  const [filter, setFilter] = useState("7 days");
+  const [isConsultationsLoading, setIsConsultationsLoading] = useState(false);
+  const handleFilterChange = (filter: string) => {
+    setFilter(filter);
+  };
+
   return (
     <div>
       <ModuleNav />
-      <section className="flex flex-col gap-4 p-4">
-        <div className="flex flex-col gap-4 rounded-lg p-4 border border-gray-200 shadow-sm">
-          <AtGalance />
-          <TitleBody title="Insights">
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col gap-4 sm:flex-row">
-                <BaseCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="plan-chat" size={12} />
-                      <Typography variant="caption">CONSULTATIONS</Typography>
-                    </div>
-                  }
-                  className="flex-[2] "
-                >
-                  <Consulations />
-                </BaseCard>
-                <BaseCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="plan-chat" size={12} />
-                      <Typography variant="caption">VS PAST PERIOD</Typography>
-                    </div>
-                  }
-                  className="flex-[1] "
-                >
-                  <PastPeriod />
-                </BaseCard>
-                <BaseCard
-                  title={
-                    <div className="flex flex-row gap-2 items-center">
-                      <Icon type="plan-chat" size={12} />
-                      <Typography variant="caption">FORCASTING</Typography>
-                    </div>
-                  }
-                  titleClassName="text-white"
-                  className="bg-red-500 flex-[1]"
-                >
-                  <div>
-                    <div className="flex gap-2 ">
-                      <Typography variant="h1">+15%</Typography>
-                      <Icon type="trending-up" />
-                    </div>
-                    <Typography variant="body" className="text-white">
-                      forecasted increase in your sales closed by the end of the
-                      current month
-                    </Typography>
-                  </div>
-                  <div>
-                    <div className="flex gap-2 ">
-                      <Typography variant="h1">+20%</Typography>
-                      <Icon type="trending-up" />
-                    </div>
-                    <Typography variant="body" className="text-white">
-                      forecasted increase in your sales closed by the end of the
-                      current month
-                    </Typography>
-                  </div>
-                </BaseCard>
-              </div>
-            </div>
-          </TitleBody>
-          <TitleBody title="Orders">
-            <div className="overflow-x-auto">
-              <OrdersTable />
-            </div>
-          </TitleBody>
+      <Section className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 rounded-lg p-2 sm:p-4 border border-gray-200 shadow-sm">
+          <AtGalance
+            onFilterChange={handleFilterChange}
+            filterOptions={filterOptions}
+          />
+          <Insights filter={filter} />
+          <Orders />
         </div>
-      </section>
+      </Section>
     </div>
   );
 }
